07eee1412bef13dbf385373731ff0639c703483e
hugosenari <hugosenari@gmail.com>
2022-03-22 00:27:13 -0300

    chore: re-index ../nimpkgs/c/criterion/0_1_2

--- 0_1_2/flake.nix
+++ 0_1_2/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-criterion-0_1_2.flake = false;
   inputs.src-criterion-0_1_2.owner = "disruptek";
-  inputs.src-criterion-0_1_2.ref   = "refs/tags/0.1.2";
+  inputs.src-criterion-0_1_2.ref   = "0_1_2";
   inputs.src-criterion-0_1_2.repo  = "criterion";
   inputs.src-criterion-0_1_2.type  = "github";
   
